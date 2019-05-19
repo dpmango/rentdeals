4f4ede05efc098aa2705fb7661ee27a31af31fa2
Sergey Khmelevskoy <xs290@me.com>
2019-05-19 17:44:29 +0300

    ommit imagemin

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -5,10 +5,12 @@ var imagemin = require('gulp-imagemin');
 var config = require('../config.js');
 
 gulp.task('images', function() {
-  return gulp
-    .src([config.src.img + '/**/*.{jpg,png,jpeg,svg,gif}', '!' + config.src.img + '/svgo/**/*.*'])
-    .pipe(config.production ? cache(imagemin({ interlaced: true })) : util.noop())
-    .pipe(gulp.dest(config.dest.img));
+  return (
+    gulp
+      .src([config.src.img + '/**/*.{jpg,png,jpeg,svg,gif}', '!' + config.src.img + '/svgo/**/*.*'])
+      // .pipe(config.production ? cache(imagemin({ interlaced: true })) : util.noop())
+      .pipe(gulp.dest(config.dest.img))
+  );
 });
 
 gulp.task('images:watch', function() {
